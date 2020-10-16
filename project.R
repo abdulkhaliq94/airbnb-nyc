b829dcc7c3ad203f6cd8ec7716011cb06fb0beac
Abdul Khaliq <abdulkhaliq94@gmail.com>
2020-10-16 00:55:19 -0400

    computed measures of center and spread

--- project.R
+++ project.R
@@ -28,9 +28,43 @@ barplot(table(airbnb$room_type), main = "Room Types Guests Prefer",
                                   xlab = "Types of Rooms",
                                   ylab = "Most Frequently Preferred")
 
+# removing outliers
+airbnb_price <- airbnb$price[airbnb$price <= 500]
+airbnb_price
 
 # removed outliers 
 # much better plot with left skewed graph
 hist(airbnb_price, breaks=100, main = "Price Distributions",
                                       xlab = "Price",
                                       ylab = "Most Frequent Charges")
+
+
+# mean
+mean(airbnb$price) # mean = 152.7207
+
+# trimmed mean
+mean(airbnb$price, trim = 0.05) # trimmed mean = 127.4977
+
+# median
+median(airbnb$price) # median = 106
+
+# variance
+var(airbnb$price) # var = 57674.03
+
+# standard deviation
+sd(airbnb$price) # standard deviation = 240.1542
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
